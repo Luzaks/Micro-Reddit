ffbe80e1dbab6e98f0416dad0cc4baf8a30d2e10
meli castorena <mcastorena0316@gmail.com>
2020-01-08 13:02:12 -0600

    Create GitIgnore

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-    validates :title,  presence: true, length: { maximum: 150 }
+  validates :title, presence: true, length: { maximum: 150 }
 
-    validates :description, presence: true, length: { maximum: 300 }
-    validates :user_id, presence: true
-            
-    belongs_to :user
-    has_many :comments
+  validates :description, presence: true, length: { maximum: 300 }
+  validates :user_id, presence: true
+
+  belongs_to :user
+  has_many :comments
 end
